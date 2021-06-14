9ee163296d79b55d37cf2c217a07544c55e10f37
Tom Metz <tommetz1@hotmail.de>
2021-06-14 12:50:24 +0200

    "Merged folders"

--- VTEg/mainwindow.cpp
+++
@@ -1,7 +0,0 @@
-//
-// Created by tomme on 14.06.2021.
-//
-
-#include "mainwindow.h"
-
-
--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "D:/Nextcloud/BNDSummerOfCode/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "D:/Nextcloud/BNDSummerOfCode/VTEg/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
