444d82cf14c33277b9974331d70ace6822472b49
BuildTools <unconfigured@null.spigotmc.org>
2021-08-03 18:23:18 +0200

    Initial public commit

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -226,6 +226,7 @@ void MainWindow::on_actionOpen_triggered() {
     currentTime = 0;
     playerState = QMediaPlayer::PlayingState;
     on_actionPlay_triggered();
+    //Test
 
     /*
      * ToDo: Write used videos into a SQLite DB and save a reference to their CSV file, alternatively, just write into CSV files
